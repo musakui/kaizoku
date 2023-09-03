5a4f45d3304d5fcee6bfe2c7a3acb52dcb0c7fc0
Shun Git <kwokshungit@gmail.com>
2023-09-03 17:01:59 +0800

    expose decryptor

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -5,12 +5,23 @@ export interface VideoOptions {
 	/** DRM key */
 	key?: string | ArrayBufferLike
 
-	/** function to get chunks from chunklist */
-	getChunks?: (cl: string) => string[]
+	/** custom decryptor function */
+	decrypt?: (buf: ArrayBuffer) => Promise<ArrayBuffer>
+
+	/** parse chunks from chunklist */
+	parseChunks?: (cl: string) => string[]
 
 	/** function to select chunklist */
 	selectChunklist?: (cls: string[]) => string
 
-	/** function to get chunklist urls from playlist */
-	getChunkLists?: (pl: string) => string[]
+	/** parse chunklist urls from playlist */
+	parseChunklists?: (pl: string) => string[]
+}
+
+export interface VideoChunk {
+	/** chunk src */
+	url: string
+
+	/** size (bytes) */
+	size?: number
 }
